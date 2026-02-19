a9530596ab85db1f304d827fef3c23c306cbce41
bismalaja <ismalajabiorni@gmail.com>
2026-02-19 10:06:22 +0100

    Fix set_admin_password.py to create superuser if missing

--- set_admin_password.py
+++ set_admin_password.py
@@ -6,7 +6,11 @@ django.setup()
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
-u = User.objects.get(username='admin')
-u.set_password('admin')
-u.save()
-print('Admin password set successfully!')
+try:
+    u = User.objects.get(username='admin')
+    u.set_password('admin')
+    u.save()
+    print('Admin password updated successfully!')
+except User.DoesNotExist:
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    print('Admin user created successfully!')
