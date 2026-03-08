2e5889f9f27df50fe124a78b03d42ff48b778911
bismalaja <bismalaja@github.com>
2026-03-08 21:11:46 +0100

    Changed static basedir

--- valorant_profile/settings.py
+++ valorant_profile/settings.py
@@ -91,6 +91,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "profiles/static"),
 ]
